5c797e98b28f7226189365b54e645927fd056ef2
semajh <jamesharrison2262@gmail.com>
2026-09-15 18:11:36 +0100

    Created an eventcontroller with event and eventid endpoints

---
+++ IomEvents.Api/Controllers/EventController.cs
@@ -0,0 +1,39 @@
+﻿using IomEvents.Domain;
+using IomEvents.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IomEvents.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class EventsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public EventsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/events
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Event>>> GetEvents()
+    {
+        return await _context.Events.ToListAsync();
+    }
+
+    // GET: api/events/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Event>> GetEvent(Guid id)
+    {
+        var @event = await _context.Events.FindAsync(id);
+
+        if (@event == null)
+        {
+            return NotFound();
+        }
+
+        return @event;
+    }
+}
---
+++ IomEvents.Infrastructure/Migrations/20260915170550_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IomEvents.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    title = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: false),
+                    startDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    location = table.Column<string>(type: "text", nullable: false),
+                    Category = table.Column<string>(type: "text", nullable: false),
+                    SourceUrl = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
