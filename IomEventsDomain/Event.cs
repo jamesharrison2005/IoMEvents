4684151e5c6715f29630d818379314b190f1151d
semajh <jamesharrison2262@gmail.com>
2026-09-15 17:12:50 +0100

    created events class domain

---
+++ IomEventsDomain/Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IomEvents.Domain
+{
+    internal class Event
+    {
+        public Guid id { get; set; }
+        public String title { get; set; } = string.Empty;
+        public String description { get; set; } = string.Empty;
+        public DateTime startDate { get; set; }
+        public String location { get; set; } = string.Empty;
+        public String Category { get; set; } = string.Empty;
+        public String SourceUrl { get; set; } = string.Empty;
+    }
+}
